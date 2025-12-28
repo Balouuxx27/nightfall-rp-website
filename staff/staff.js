67f104b9b06af429c64a8c353dcf4e3c101b1768
Balouuxx <balouuxx@protonmail.com>
2025-12-29 00:49:10 +0100

    Fix: minX à -3950 (gauche corrigé)

--- staff/staff.js
+++ staff/staff.js
@@ -122,9 +122,9 @@
     // Image: map gta.jpg (5944x8075 pixels)
     // Coordonnées finales ajustées
     // Y: -3973 à +8335 (12308 unités)
-    // X: -3850 à +5062 (8912 unités)
+    // X: -3950 à +5062 (10012 unités)
     
-    const minX = -3850;
+    const minX = -3950;
     const maxX = 5062;
     const minY = -3973;
     const maxY = 8335;
