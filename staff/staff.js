d2429ec0ede120bf99e94652febad8a09da31e52
Balouuxx <balouuxx@protonmail.com>
2025-12-28 18:56:14 +0100

    Fix: Agrandissement maxX à 4200 pour corriger décalage

--- staff/staff.js
+++ staff/staff.js
@@ -122,10 +122,10 @@
     // Image: map gta.jpg (5944x8075 pixels)
     // Coordonnées finales ajustées
     // Y: -4000 à +8000 (12000 unités)
-    // X: -3800 à +3200 (7000 unités) - maxX encore réduit (trop d'amplitude à droite)
+    // X: -3800 à +4200 (8000 unités) - maxX agrandi pour corriger le décalage
     
     const minX = -3800;
-    const maxX = 3200;
+    const maxX = 4200;
     const minY = -4000;
     const maxY = 8000;
     
